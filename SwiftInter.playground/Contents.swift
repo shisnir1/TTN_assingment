f6849647ded9c975564cb6df7a54c8e1bf75aa9c
shishir Singh <60691751+shisnir1@users.noreply.github.com>
2020-02-27 16:05:44 +0530

    NewCommit

--- SwiftInter.playground/Contents.swift
+++ SwiftInter.playground/Contents.swift
@@ -21,49 +21,35 @@ let result = siftBeans(fromGroceryList: ["green beans",
 print(result.beans)
 print(result.otherGroceries)
 //Make a calculator class with a function name “equals” that take an enum case as value like multiply, subtraction, addition, square root, division.
-enum calculation{
-    
-    case Addision
-    case multiplication
-    case divide
-    case sub
-}
-
 class Calculator{
-    var a = 10
-           var b = 11
-           var c = 0
-    func equals1(cal :calculation) {
-       
-        switch cal {
-            
-        case .Addision:
-            c=a+b
-            print(c)
-            break
-            
-        case .divide:
-            c=a%b
-            print(c)
-            break
-            
-        case .multiplication:
-            c=a*b
-            print(c)
-            break
-            
-        default:
-            print(a-b)
-            break
-            
-        }
-
-        equals1(cal: .Addision)
-
+    
+    enum Operations {
+        case addition(Int, Int)
+        case subtraction(Int, Int)
+        case multiplication(Int, Int)
+        case division(Int, Int)
     }
     
+    func equals(enumArgument : Operations) -> Int {
+        switch enumArgument {
+        case .addition(let a, let b):
+            return a + b
+        case .subtraction(let a, let b):
+            return a - b
+        case .multiplication(let a, let b):
+            return a*b
+        case .division(let a, let b):
+            return a/b
+        }
+    }
+    
+    func equals(argumentAs function : (Int, Int) -> Int, num1 : Int, num2 : Int) -> Int {
+        return function(num1, num2)
+    }
     
 }
+var abc = Calculator()
+let testCaseForQuestion2 = abc.equals(enumArgument: .addition(1, 2))
 
 //Question 3 -
 //
@@ -89,7 +75,4 @@ struct Operation {
    }
 
 
-let abc2 = abc.equals(argumentAs: Operation.addition(a:b:), num1: 1, num2: 2)
-
-
-
+let abc2 = abc.equals(argumentAs: Operation.multiplication(a:b:), num1: 1, num2: 2)
